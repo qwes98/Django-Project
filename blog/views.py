0a2125d76520e02ad92a632091cf46d983a3a8f5
Jiwon <qwes8873@gmail.com>
2021-06-29 19:03:44 +0900

    Modify view file

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse('<h1>Hello</h1>')
+	return HttpResponse('<h1>Hello World</h1>')
 
 
 def post_list(request):
